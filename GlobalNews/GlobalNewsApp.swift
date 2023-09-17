92e1655e105492084a142ab249bcc48b10f4d3e6
adebayu <adebayu17@gmail.com>
2023-09-17 18:41:22 +0700

    Initial Commit

---
+++ GlobalNews/GlobalNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalNewsApp.swift
+//  GlobalNews
+//
+//  Created by mymac on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GlobalNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
